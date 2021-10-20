8e15023d877402fae7542f706dcca8bf8281595f
Changseo Jang <changseo.jang@riiid.co>
2021-10-20 17:20:49 +0900

    remove unneeded

--- Formula/pbkit.rb
+++ Formula/pbkit.rb
@@ -5,7 +5,6 @@ class Pbkit < Formula
   sha256 "3b2ac0c2c047b50651c809b2b03b565dcc35d280e3177b577c72bc455cccac74"
   license any_of: ["Apache-2.0", "MIT"]
   depends_on "deno"
-  bottle :unneeded
   def install
     mkdir_p libexec
     system "mv ./* #{libexec}/"
--- Formula/pollapo.rb
+++ Formula/pollapo.rb
@@ -5,7 +5,6 @@ class Pollapo < Formula
   sha256 "84bc7f9f22f6818256b60c603815f64970dd74c98bb1c1a6637f1e345690fc4f"
   license any_of: ["Apache-2.0", "MIT"]
   depends_on "deno"
-  bottle :unneeded
   deprecate! date: "2021-06-13", because: "is replaced by the pbkit package.\nWhen you install the pbkit package, the pollapo command is also installed"
   def install
     mkdir_p libexec
