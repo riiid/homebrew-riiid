b6041394fcbeb0efcf38db69b03967d127975f82
안재원 (Jaewon Ahn) <jaewon.ahn@riiid.co>
2022-03-25 15:43:34 +0900

    Release toolbelt/0.0.8

--- Formula/toolbelt.rb
+++ Formula/toolbelt.rb
@@ -1,7 +1,7 @@
 class Toolbelt < Formula
   desc "cli utilities for riiid engineering"
   homepage "https://github.com/riiid/toolbelt"
-  url "https://github.com/riiid/toolbelt/archive/v0.0.7.tar.gz"
+  url "https://github.com/riiid/toolbelt/archive/v0.0.8.tar.gz"
   sha256 "9699abbbca84121e7d63e54244f9c3da210662e086776ba58ca39766f9bc2d51"
   license any_of: ["Apache-2.0", "MIT"]
   depends_on "deno"
