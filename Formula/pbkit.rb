71a041976f731183bb0a87d623aee4d20a025058
Yongwook Choi <hyperflow@kakao.com>
2022-07-13 16:55:25 +0900

    Deprecate riiid/riiid/pbkit (#10)

--- Formula/pbkit.rb
+++ Formula/pbkit.rb
@@ -1,10 +1,11 @@
 class Pbkit < Formula
   desc "Protobuf toolings"
   homepage "https://github.com/pbkit/pbkit"
-  url "https://github.com/pbkit/pbkit/archive/v0.0.42.tar.gz"
-  sha256 "2d4be005e9f917d8b131338a88e169eec52f5a8ed391e94025e584f35a1fcbce"
+  url "https://github.com/pbkit/pbkit/archive/refs/tags/v0.0.51.tar.gz"
+  sha256 "7103a0a72182db8513390b47e6fa3012c59de49570ca96e32d591ca60e87c899"
   license any_of: ["Apache-2.0", "MIT"]
   depends_on "deno"
+  deprecate! date: "2022-07-13", because: "is moved to pbkit/tap/pbkit. Install pbkit/tap/pbkit instead."
   def install
     mkdir_p libexec
     system "mv ./* #{libexec}/"
